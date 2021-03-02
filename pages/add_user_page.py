5a5e30712bf40fbc29035cd98dbc5356a80c01e6
AnaitP <petrosyan-a@yandex.ru>
2021-03-02 23:38:58 +0300

    Добавление селекторов Add_user

---
+++ pages/add_user_page.py
@@ -0,0 +1,36 @@
+from .base_page import BasePage
+from .locators import AddUserPageLocators
+
+first_name_test = 'Тестовый'
+last_name_test = 'Тест'
+user_login_test = 'Тест'
+user_email = 'test@mail.ru'
+user_role = 'Alpha'
+password_test = '123'
+new_login = 'test'
+new_password = '321'
+
+class AddUserPage(BasePage):
+
+    def fill_add_user_fields(self):
+        first_name = self.browser.find_element(*AddUserPageLocators.FIRST_NAME)
+        first_name.send_keys(first_name_test)
+        last_name = self.browser.find_element(*AddUserPageLocators.LAST_NAME)
+        last_name.send_keys(last_name_test)
+        user_login = self.browser.find_element(*AddUserPageLocators.USER_LOGIN)
+        user_login.send_keys(user_login)
+        active = self.browser.find_element(*AddUserPageLocators.ACTIVE)
+        active.click()
+        email = self.browser.find_element(*AddUserPageLocators.EMAIL)
+        email.send_keys(user_email)
+        role = self.browser.find_element(*AddUserPageLocators.ROLE)
+        role.click()
+        role.send_keys(user_role)
+        role.send_keys(Keys.ENTER)
+        password = self.browser.find_element(*AddUserPageLocators.PASSWORD)
+        password.send_keys(password_test)
+        conf_password = self.browser.find_element(*AddUserPageLocators.CONFPASSWORD)
+        conf_password.send_keys(password_test)
+        save_button = self.browser.find_element(*AddUserPageLocators.SAVE_BUTTON)
+        save_button.click()
+
--- pages/users_page.py
+++ pages/users_page.py
@@ -8,3 +8,6 @@ class UsersPage(BasePage):
         add_user_button = self.browser.find_element(*UsersPageLocators.ADD_USER_BUTTON)
         add_user_button.click()
 
+    def user_should_be_added (self)
+        assertion = browser.find_element_by_css_selector('#alert-container > div').text
+        assert assertion == '×\nДобавлено строк'
